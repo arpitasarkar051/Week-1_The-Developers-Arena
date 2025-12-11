6bd44bd2a5f0ead9f14c13b10062b79d3f633472
Arpita Sarkar <95957364+arpitasarkar051@users.noreply.github.com>
2025-12-11 22:18:51 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your favorite hobby: ")
+
+
+print("\n--- Personal Introduction ---")
+print(f"Welcome, {name}!")
+print(f"You are {age} years old and you enjoy {hobby}.")
+print("Nice to meet you!")
+
+
+
